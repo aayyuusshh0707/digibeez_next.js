5ae8a050d72aedf3ea9b5bace9a0bd585b34af0f
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-01-25 10:48:44 +0530

    Add ESLint configuration to ignore warnings during builds and format image domains array

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Add the eslint configuration here
+  },
   images: {
-    domains: ["images.unsplash.com", "assets.aceternity.com", "cdn.codingninjas.com"], // Combine domains in a single array
+    domains: [
+      "images.unsplash.com",
+      "assets.aceternity.com",
+      "cdn.codingninjas.com",
+    ], // Combine domains into a single array
   },
 };
 
